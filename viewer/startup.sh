9ab14aa85627b81fd8dad25e47c16d77160f14b5
terryaguiar <terryaguiar@suzano.com.br>
2024-09-29 20:53:27 -0300

    Changed default port for Essential Viewer from 8080 to 80 for Azure Web App compatibility.

--- viewer/startup.sh
+++ viewer/startup.sh
@@ -3,6 +3,6 @@ echo "Starting SSH ..."
 service ssh start
 
 # Start oauth2-proxy in background
-nohup  /opt/oauth2-proxy --custom-sign-in-logo '-' --http-address 0.0.0.0:8080 > /var/log/oauth2-proxy.log 2>&1 &
+nohup  /opt/oauth2-proxy --custom-sign-in-logo '-' --http-address 0.0.0.0:80 > /var/log/oauth2-proxy.log &
 
 catalina.sh run
